16c2f15979805fd1e3980521fcaab0cba382d945
ahmed.gondal <ahmednadeemgondal92@gmail.com>
2026-03-12 14:27:39 +0500

    Iresult interface for accessing helper methods for status code, serialization and messaging

--- DeepDiveReturnTypes/Program.cs
+++ DeepDiveReturnTypes/Program.cs
@@ -6,22 +6,36 @@ var app = builder.Build();
 //Implicit routing and enpoint handling
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/emplyoees", () =>
+app.MapGet("/employees", () =>
 {
     var employees = EmployeesRepository.GetEmployees();
     // As an object is being returned, it will be converted
     // to JSON by the framework
-    return employees;
+
+    // The results.ok will also provide the appropiate status
+    // code along with serialization, this is currently returning
+    // an array of objects.
+    // This is returning an IResult type.
+    return Results.Ok(employees);
+
+    //// Another more robust option is to retun Typed results
+    //// The framework will extract the tpe when building documentation for example.
+    //return TypedResults.Ok(employees);
 });
 
 app.MapPost("/employees", (Employee employee) =>
 {
     if (employee is null || employee.Id <= 0)
     {
-        return "Provided employee is invalid";
+        //// The following codewould still forward astatus code of 200 Ok
+        //// as a response is being sent albeit an undesirable one.
+        //return "Provided employee is invalid";
+        // Results.BadRequest() is the helper function that allows sending
+        // the correct status code along with a message if needed.
+        return Results.BadRequest("Provided employee is invalid") ;
     }
     EmployeesRepository.AddEmployee(employee);
-    return "Employee added successfully";
+    return Results.Ok("Employee added successfully");
 }).WithParameterValidation();
 
 
